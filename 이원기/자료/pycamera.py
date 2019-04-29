4f0f6d5f01e8b181dc522fa718453d5da9f100fa
eo <shkuei773@gmail.com>
2019-04-29 18:46:20 +0900

    up

---
+++ 이원기/자료/pycamera.py
@@ -0,0 +1,18 @@
+import time
+import picamera
+import RPi.GPIO as GPIO
+import datetime
+
+day=now.strftime('%Y-%m-%d,%HH%MM%S')
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17,GPIO.IN,GPIO.PUD_UP)
+
+with picamera.PiCamera() as camera:
+    camera.start_preview()
+    
+    while true:
+        GPIO.wait_for_edge(17,GPIO.FALLING)
+        time.sleep(10)
+        camera.capture('/home/pi/',nowDatetime,'.jpg')
+    camera.stop_preview()
